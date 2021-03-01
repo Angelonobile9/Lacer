cec40ddadd0daceee6b5ed230fa6526cbb3559ed
Angelonobile9 <78024505+Angelonobile9@users.noreply.github.com>
2021-03-01 19:00:31 +0100

    autorizzazioni e associazioni per user e contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_151539) do
+ActiveRecord::Schema.define(version: 2021_03_01_163652) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "prezzo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
